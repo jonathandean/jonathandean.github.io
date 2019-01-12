fab89fd6688fcf6929bdd333345b6ec629a5f89b
Jonathan Dean <jon@jonathandean.com>
2019-01-12 14:06:32 -0500

    Setting number of tweets to fetch to 0 will load as many tweets as allowed by the API

--- functions/top-tweets/function.rb
+++ functions/top-tweets/function.rb
@@ -16,7 +16,25 @@ class TopTweets
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    timeline = client.user_timeline(ENV.fetch("TWITTER_USERNAME"), count: ENV.fetch("NUMBER_OF_TWEETS"), exclude_replies: true, include_rts: false)
+    def collect_with_max_id(collection=[], max_id=nil, &block)
+      response = yield(max_id)
+      collection += response
+      response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+    end
+
+    def client.get_all_tweets(user, exclude_replies: , include_rts: )
+      collect_with_max_id do |max_id|
+        options = {count: 200, exclude_replies: exclude_replies, include_rts: include_rts}
+        options[:max_id] = max_id unless max_id.nil?
+        user_timeline(user, options)
+      end
+    end
+
+    timeline = if ENV.fetch("NUMBER_OF_TWEETS").to_i > 0
+                client.user_timeline(ENV.fetch("TWITTER_USERNAME"), count: ENV.fetch("NUMBER_OF_TWEETS"), exclude_replies: true, include_rts: false)
+               else
+                 client.get_all_tweets(ENV.fetch("TWITTER_USERNAME"), exclude_replies: true, include_rts: false)
+               end
 
     timeline.each do |tweet|
       favorited = tweet.attrs[:favorited]
